93b86d010dc3e921cc08962fd06c35675a717c04
movie99 <33032293+movie99@users.noreply.github.com>
2023-10-03 15:54:02 +0000

    10/3/2023 at 9:53

--- Dustyn/js/script.js
+++ Dustyn/js/script.js
@@ -1,2 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const profileForm = document.getElementById('profileForm');
 
-console.log("Testing if JavaScript module is linked");
+    profileForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        // Get form data
+        const formData = new FormData(profileForm);
+        const formObject = {};
+
+        // Convert form data to JSON object
+        formData.forEach((value, key) => {
+            formObject[key] = value;
+        });
+
+        // Send the formObject as JSON to the server (you can use fetch or AJAX for this)
+        console.log(JSON.stringify(formObject)); // For demonstration purposes
+
+        // Reset the form or perform any additional actions
+        profileForm.reset();
+    });
+});
--- alex/js/script.js
+++ alex/js/script.js
@@ -1,3 +1,65 @@
 
 console.log("Testing if JavaScript module is linked");
 console.log("Changes my dudes")
+
+
+const apiUrl = 'https://teamserver.alexhernandez11.repl.co/user_info';
+
+// Data to send in the POST request
+const postData = {
+  //password: 'secret',
+  //ip: 'exampleIP',
+  //port: 'examplePort',
+  user_name: "alex23",
+  hobbies: "love to play video games",
+  age: 21,
+  height: 5.5,
+
+};
+
+// Define options for the fetch request
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(postData),
+};
+
+
+// Send the POST request to the server
+fetch(apiUrl, requestOptions)
+  .then(response => {
+    if (response.ok) {
+      return response.json(); // Assuming the server responds with JSON data
+    } else {
+      throw new Error('Invalid ');
+    }
+  })
+  .then(data => {
+    console.log('Response from the server:', data);
+    // Handle the server's response data here
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+/*
+// Send the POST request to the server
+fetch(apiUrl, requestOptions)
+  .then(response => {
+    if (response.ok) {
+      return response.json(); // Assuming the server responds with JSON data
+    } else {
+      throw new Error('Invalid password');
+    }
+  })
+  .then(data => {
+    console.log('Response from the server:', data);
+    // Handle the server's response data here
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+*/
